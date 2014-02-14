e2197626d958ffe994a1adeb6eca80c866e879c4
wotupset <wotupset@gmail.com>
2014-02-14 10:14:43 +0800

    Auto-committed on 2014/02/14 星期五

---
+++ 新增文字文件.txt.bat
@@ -0,0 +1,22 @@
+rem @echo off
+echo type "commit" or "update"
+cd "curl"
+ 
+set GIT_PATH="C:\Program Files\Git\bin\git.exe"
+set BRANCH = "origin"
+ 
+:P
+set ACTION=
+set /P ACTION=Action: %=%
+if "%ACTION%"=="c" (
+  %GIT_PATH% add -A
+	%GIT_PATH% commit -am "Auto-committed on %date%"
+	%GIT_PATH% pull %BRANCH%
+	%GIT_PATH% push %BRANCH%
+)
+if "%ACTION%"=="u" (
+	%GIT_PATH% pull %BRANCH%
+)
+if "%ACTION%"=="exit" exit /b
+pause
+goto P
