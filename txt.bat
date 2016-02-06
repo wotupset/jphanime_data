c13dbccfcd36d306a0865dcfa54affe3cdb23f51
wotupset <wotupset@gmail.com>
2016-02-06 23:00:15 +0800

    Auto-committed on 2016/02/06 星期六

--- txt.bat
+++ txt.bat
@@ -1,3 +1,28 @@
+
+set GIT_PATH="..\PortableGit\bin\git.exe"
+set BRANCH = "origin"
+
+%GIT_PATH% config --list
+%GIT_PATH% --version
+%GIT_PATH% add -A
+%GIT_PATH% commit -am "Auto-committed on %date%"
+%GIT_PATH% pull %BRANCH%
+%GIT_PATH% push %BRANCH%
+pause
+exit
+
+
+pause
+exit
+
+
+git config --global user.name "wotupset"
+
+git config --global user.email "wotupset@gmail.com"
+
+git config --list
+
+
 @echo off
 echo type "c (commit)" or "u (update)"
 cd .
