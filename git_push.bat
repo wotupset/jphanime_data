a6e53f830b165e99133a0d7f7f9a6d00ec3d00ad
wotupset <wotupset@gmail.com>
2018-02-10 23:21:10 +0800

    Auto-committed on 2018/02/10 週六

--- git_push.bat
+++ git_push.bat
@@ -1,4 +1,4 @@
-git config ¡Vglobal core.quotepath false
+git config --global core.quotepath false
 git config --global gui.encoding utf-8         
 git config --global i18n.commit.encoding utf-8  
 git config --global i18n.logoutputencoding utf-8  
